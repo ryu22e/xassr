423ef57f9a9012089e42e1626c81c3baa2cb848e
ryu22e <ryu22e@gmail.com>
2012-10-08 22:36:10 +0900

    Modify TIME_ZONE and LANGUAGE_CODE

--- xassr/settings/common.py
+++ xassr/settings/common.py
@@ -61,10 +61,10 @@ DATABASES = {
 
 ########## GENERAL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#time-zone
-TIME_ZONE = 'America/Los_Angeles'
+TIME_ZONE = 'Asia/Tokyo'
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#language-code
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#site-id
 SITE_ID = 1
