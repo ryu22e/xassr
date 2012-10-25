eb4c4b7b8e5849447e8b2ac9ecb7322fb2a67fbe
ryu22e <ryu22e@gmail.com>
2012-10-25 23:54:02 +0900

    Add '$' to end of url

--- xassr/apps/accounts/urls.py
+++ xassr/apps/accounts/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import patterns, url
 
 # See: https://docs.djangoproject.com/en/dev/topics/http/urls/
 urlpatterns = patterns('',
-    url(r'^signup/', 'xassr.apps.accounts.views.signup'),
+    url(r'^signup/$', 'xassr.apps.accounts.views.signup'),
 )
