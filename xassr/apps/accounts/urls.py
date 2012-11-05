e87b0a349996af19a60dec395c5bbf51700c325c
ryu22e <ryu22e@gmail.com>
2012-11-06 00:22:52 +0900

    Add sign out

--- xassr/apps/accounts/urls.py
+++ xassr/apps/accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     url(r'^signup/$', 'xassr.apps.accounts.views.signup'),
     url(r'^verify/(?P<token>.+)/$', 'xassr.apps.accounts.views.verify'),
     url(r'^signin/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/signin.html'}),
+    url(r'^signout/$', 'django.contrib.auth.views.logout_then_login', {'login_url': '/accounts/signin/'}),
 )
